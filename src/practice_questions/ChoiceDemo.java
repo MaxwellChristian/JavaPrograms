476eea96f06da2f986f4f429c8063e38b9dc9f75
Maxwell Christian <112983367+MaxwellChristian@users.noreply.github.com>
2022-10-04 08:39:31 -0600

    Maxwell Christian

---
+++ src/practice_questions/ChoiceDemo.java
@@ -0,0 +1,21 @@
+
+/*
+*Write a JAVA program which shows a menu option to the user and performs the actions according to the choice of the user
+1. convert an int value to binary and display the same
+2. find the length of a string using for loop
+3. Input the number of students and then read name and score for that each student. After all inputs display the name of the student with highest score
+4. convert a hex string to binary [in octets i.e. space after each 8 bits]
+5. Enter a 10 digit SIN number and print in the provided format [e.g 123123456 : 123-12-3456]
+*
+* */
+
+package practice_questions;
+
+public class ChoiceDemo {
+
+    public static void main(String []args){
+
+
+    }
+
+}
