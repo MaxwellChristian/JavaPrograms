5cf8fea219cb76cefb1771787f27ff7bc6d65f46
Maxwell Christian <112983367+MaxwellChristian@users.noreply.github.com>
2022-10-07 15:22:23 -0600

    Maxwell Christian

--- src/practice_questions/ChoiceDemo.java
+++ src/practice_questions/ChoiceDemo.java
@@ -3,7 +3,7 @@ package practice_questions;
 *Write a JAVA program which shows a menu option to the user and performs the actions according to the choice of the user
 1. convert an int value to binary and display the same
 2. reverse a string using for loop
-3. Input the number of students and then read name and score for that each student. After all inputs display the name of the student with highest score
+3. Input the number of students and then read name and score for that each student. After all inputs display the name of the student with the highest score
 4. convert a hex string to binary [in octets i.e. space after each 8 bits]
 5. Enter a 9 digit SIN number and print in the provided format [e.g 123123456 : 123-12-3456]
 *
@@ -49,9 +49,9 @@ public class ChoiceDemo {
                     // int to binary
 
                     System.out.print("Enter int number: ");
-                    int numnber = input.nextInt();
+                    int number = input.nextInt();
 
-                    System.out.println(numnber + " : " + Integer.toBinaryString(numnber));
+                    System.out.println(number + " : " + Integer.toBinaryString(number));
 
                     break;
                 case 2:
