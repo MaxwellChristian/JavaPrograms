8aea4abc48e087aac814a8f1f72c58ee63580234
Maxwell Christian <112983367+MaxwellChristian@users.noreply.github.com>
2022-10-07 22:56:27 -0600

    Maxwell Christian

--- src/practice_questions/ChoiceDemo.java
+++ src/practice_questions/ChoiceDemo.java
@@ -1,10 +1,14 @@
 package practice_questions;
 /*
-*Write a JAVA program which shows a menu option to the user and performs the actions according to the choice of the user
+*
+* Write a JAVA program which shows a menu option to the user and
+* performs the actions according to the choice of the user.
+    * Choice options displayed are
 1. convert an int value to binary and display the same
 2. reverse a string using for loop
-3. Input the number of students and then read name and score for that each student. After all inputs display the name of the student with the highest score
-4. convert a hex string to binary [in octets i.e. space after each 8 bits]
+3. Input the number of students and then read name and score for that each student.
+        After all inputs display the name of the student with the highest score
+4. Convert a hex string to binary [in octets i.e. space after each 8 bits]
 5. Enter a 9 digit SIN number and print in the provided format [e.g 123123456 : 123-12-3456]
 *
 * Do not exit the program till the user enters [0]
@@ -132,7 +136,7 @@ public class ChoiceDemo {
                     String sinNumber = input.next();
 
                     if (sinNumber.length() != MAX_DIGITS_IN_SIN_NUMBER) {
-                        System.out.println("SIN number must be of 9 digits");
+                        System.out.println("SIN number must be of " + MAX_DIGITS_IN_SIN_NUMBER + " digits");
                     } else {
                         String formatterSinNumber = sinNumber.substring(0, 3)
                                 + "-" + sinNumber.substring(3, 5)
