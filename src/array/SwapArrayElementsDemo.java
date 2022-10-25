283d081a24df5516bd0678526a5980befd2b98fb
Maxwell Christian <112983367+MaxwellChristian@users.noreply.github.com>
2022-10-25 14:27:18 -0600

    Maxwell Christian

--- src/array/SwapArrayElementsDemo.java
+++ src/array/SwapArrayElementsDemo.java
@@ -14,24 +14,28 @@ public class SwapArrayElementsDemo {
         System.out.println("Value 2 : " + arr[1]);
     }
 
+    // swap method with ONE parameter i.e. 1 dimensional int array
+    // overloaded method
     private static void swap(int[] values) {
-
-        int[] arr2 = new int[2];
-        arr2[0] = values[0];
-        arr2[1] = values[1];
 
         int temp ;
 
-//        temp = values[0];
-//        values[0] = values[1];
-//        values[1] = temp;
+//        int[] arr2 = new int[2];
+//        arr2[0] = values[0];
+//        arr2[1] = values[1];
 
-        temp = arr2[0];
-        arr2[0] = arr2[1];
-        arr2[1] = temp;
+//        temp = arr2[0];
+//        arr2[0] = arr2[1];
+//        arr2[1] = temp;
+
+        temp = values[0];
+        values[0] = values[1];
+        values[1] = temp;
 
     }
 
+    // swap method with INT String parameters
+    // overloaded method
     private static void swap(int value1, int value2) {
 
         int temp ;
@@ -42,20 +46,29 @@ public class SwapArrayElementsDemo {
 
     }
 
+    // swap method with NO parameters
+    // overloaded method
     private static void swap(){
 
     }
 
+    // swap method with TWO String parameters
+    // overloaded method
     private static void swap(String str1, String str2){
 
     }
 
+    // swap method with INT String parameters
+    // overloaded method
     private static void swap( int value1, String str1){
 
     }
 
+    // NOT ALLOWED : as there is already a swap method with TWO int parameters
+    // the RETURN type is not considered for overloaded methods
 //    private static int swap(int v1, int v2){
 //        return 0;
 //    }
+
 
 }
