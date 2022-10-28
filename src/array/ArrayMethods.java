96b5d881de76c49e5049fefaaab65f05dd807d67
Maxwell Christian <112983367+MaxwellChristian@users.noreply.github.com>
2022-10-28 09:12:47 -0600

    Maxwell Christian

--- src/array/ArrayMethods.java
+++ src/array/ArrayMethods.java
@@ -1,8 +1,39 @@
 package array;
 
+import java.util.Random;
+
 public class ArrayMethods {
 
-    static void printArray(int[] values) {
+    public static final int TYPE_INT = 0 ;
+    public static final int BUBBLE_SORT = 0;
+    public static final int SELECTION_SORT = 1;
+    public static final int ASCENDING = 0;
+    private static final int DESCENDING = 1;
+
+    public static double[] getRandomDoubleValues(int noOfValues, int maxValue) {
+
+        double[] values = new double[noOfValues];
+
+        for (int index = 0; index < values.length; index++) {
+            values[index] = new Random().nextDouble(maxValue);
+        }
+
+        return values;
+    }
+
+    public static int[] getRandomIntValues(int noOfValues, int maxValue) {
+
+        int[] values = new int[noOfValues];
+
+        for (int index = 0; index < values.length; index++) {
+            values[index] = new Random().nextInt(maxValue);
+        }
+
+        return values;
+    }
+
+    // overloaded method to print array with int values
+    public static void printArray(int[] values) {
 
         for (int currentValue :
                 values) {
@@ -11,4 +42,105 @@ public class ArrayMethods {
 
     }
 
+    // overloaded method to print array with double values
+    public static void printArray(double[] values) {
+        for (double currentValue :
+                values) {
+            System.out.print(currentValue + " ");
+        }
+    }
+
+    public static void show2DValues(int[][] matrix) {
+        int row;
+        int col;
+
+        for (row = 0 ; row < matrix.length ; row++) {
+            System.out.print("| ");
+            for( col = 0 ; col < matrix[row].length ; col++ ){
+                System.out.printf("%2d ", matrix[row][col]);
+            }
+            System.out.print("|");
+            System.out.println();
+        }
+    }
+
+    public static void sortMatrix(int[][] matrix) {
+
+        // copy all values to a 1D array
+
+        // sort the 1D array
+
+        // copy back to matrix
+
+    }
+
+    public static void sort(int algorithm, int sortOrder, int[] values) {
+
+        switch (algorithm) {
+            case BUBBLE_SORT -> {
+                bubbleSort(sortOrder, values);
+            }
+            case SELECTION_SORT -> {
+//                selectionSort(sortOrder, values);
+            }
+        }
+
+    }
+
+    private static void bubbleSort(int sortOder, int[] values){
+        int currentCounter;
+        int adjacentCounter;
+
+        // iterate over all values
+        for( currentCounter = 0 ; currentCounter < values.length ; currentCounter++ ){
+
+            // iterate over all adjacent values
+            for( adjacentCounter = currentCounter+1 ; adjacentCounter < values.length ; adjacentCounter++ ){
+
+                // compare the current value with the adjacent value
+                // as per sorting order required
+
+                switch (sortOder){
+                    case ASCENDING -> {
+                        // Ascending order
+                        if( values[currentCounter] > values[adjacentCounter] ){
+                            // swap the values
+                            swap(values, currentCounter, adjacentCounter);
+                        }
+                    }
+                    case DESCENDING -> {
+                        // DESCENDING order
+                        if( values[currentCounter] < values[adjacentCounter] ){
+                            // swap the values
+                            swap(values, currentCounter, adjacentCounter);
+                        }
+                    }
+                }
+
+            }
+
+            System.out.printf("Pass %d : ", (currentCounter+1));
+            printArray(values);
+            System.out.println();
+
+        }
+    }
+
+    private static void swap(int[] values, int position1, int position2) {
+
+        // using an extra variable
+//        int temp;
+//        temp = values[position1];
+//        values[position1] = values[position2];
+//        values[position2] = temp;
+
+        // without using extra variable;
+        // p1 : 10
+        // p2 : 5
+
+        values[position1] = values[position1] + values[position2]; // p1 : 15, p2 : 5
+        values[position2] = values[position1] - values[position2]; // p1 : 15, p2 : 10
+        values[position1] = values[position1] - values[position2]; // p1 : 5, p2 : 10
+
+    }
 }
---
+++ src/array/sorting_demos/BubbleSortDemo.java
@@ -0,0 +1,40 @@
+
+/*
+*
+* Write a program which can sort values using bubble sort algorithm
+*
+* Bubble sort:
+* - compare a value with adjacent value
+* - swap if necessary
+* - continue till end of values
+*
+* */
+
+package array.sorting_demos;
+
+import array.ArrayMethods;
+
+public class BubbleSortDemo {
+
+    public static void main(String[] args){
+
+        // generate some random int values [as per the desired count and max value]
+        int[] values = ArrayMethods.getRandomIntValues(10, 100);
+
+        System.out.print("Before Sort : ");
+        ArrayMethods.printArray(values);
+        System.out.println();
+
+        // sort the values in desired order using a specific algorithm
+        // order desired : Ascending
+        // algorithm to use : bubble sort
+
+        ArrayMethods.sort(ArrayMethods.BUBBLE_SORT, ArrayMethods.ASCENDING, values);
+
+        System.out.print("After Sort  : ");
+        ArrayMethods.printArray(values);
+        System.out.println();
+
+    }
+
+}
