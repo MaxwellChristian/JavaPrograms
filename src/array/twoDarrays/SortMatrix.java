d82db7dc0199cbac997b8a584c751b40fd30b2b6
Maxwell Christian <112983367+MaxwellChristian@users.noreply.github.com>
2022-11-01 12:27:57 -0600

    Maxwell Christian

--- src/array/twoDarrays/SortMatrix.java
+++ src/array/twoDarrays/SortMatrix.java
@@ -12,29 +12,122 @@
 
 package array.twoDarrays;
 
-import array.ArrayMethods;
+import java.util.Random;
+import java.util.Scanner;
 
 public class SortMatrix {
 
     public static void main(String []args){
 
-        int [][]matrix = {
-                {19, 29, 1, 57, 8},
-                {7, 189, 21, 7, 9},
-                {9, 9, 1, 1, 2}
-        };
+        // declaration section
 
+        // total rows desired by user
+        int totalRows;
+        // total cols desired by user
+        int totalCols;
 
-        System.out.println("Before sort");
-        ArrayMethods.printValues(matrix, 3, ' ');
+        // input line for the user
+        Scanner input = new Scanner(System.in);
+
+        // prompt the user to input desired rows
+        System.out.print("Enter rows: ");
+        totalRows = input.nextInt();
+
+        // prompt the user to input desired cols
+        System.out.print("Enter cols: ");
+        totalCols = input.nextInt();
+
+        // create a matrix of user provided rows and cols
+        int [][]matrix = new int[totalRows][totalCols];
+
+        // fill the matrix with random values
+        fillWithRandomValues(matrix, 20);
+
+        // display the matrix
+        printValues(matrix);
         System.out.println();
 
-        ArrayMethods.sortMatrix(matrix);
+        // sort the matrix
+        sortMatrix(matrix);
 
-        System.out.println("After sort");
-        ArrayMethods.printValues(matrix, 3, ' ');
-        System.out.println();
+        // display the matrix
+        printValues(matrix);
 
+    }
+
+    private static void sortMatrix(int[][] matrix) {
+        // copy all values from matrix to 1D array
+        int []values = new int[matrix.length*matrix[0].length];
+        copyValues(matrix, values);
+
+        // sort the 1D array
+        sortValues(values);
+
+        // copy values from 1D array back to matrix
+        copyValues(values, matrix);
+    }
+
+    private static void copyValues(int[] values, int[][] matrix) {
+        int index = 0;
+        for( int row = 0 ; row < matrix.length ; row++ ){
+            for ( int col = 0 ; col < matrix[row].length ; col++ ){
+                matrix[row][col] = values[index];
+                index++;
+            }
+        }
+    }
+
+    private static void sortValues(int[] values) {
+        for( int row = 0 ; row < values.length ; row++ ){
+            for (int col = row+1 ; col < values.length ; col++ ){
+                if( values[row] > values[col] ){
+                    // swap the values
+                    int temp ;
+                    temp = values[row];
+                    values[row] = values[col];
+                    values[col] = temp;
+                }
+            }
+        }
+    }
+
+    private static void copyValues(int[][] matrix, int []values) {
+
+        int index;
+
+        // copy all values from matrix to a 1D array
+        index = 0;
+        for (int[] rows : matrix) {
+            for (int colValue : rows) {
+                values[index] = colValue;
+                index++;
+            }
+        }
+
+    }
+
+    private static void fillWithRandomValues(int[][] matrix, int maxValue) {
+
+        int row;
+        int col;
+
+        for( row = 0 ; row < matrix.length ; row++ ){
+            for (col = 0 ; col < matrix[row].length ; col++ ){
+                matrix[row][col] = new Random().nextInt()%maxValue;
+            }
+        }
+
+    }
+
+    private static void printValues(int[][] matrix) {
+        int row;
+        int col;
+        for( row = 0 ; row < matrix.length ; row++ ){
+            for (col = 0 ; col < matrix[row].length ; col++ ){
+                System.out.print( matrix[row][col] + " " );
+            }
+            System.out.println();
+        }
     }
 
 }
