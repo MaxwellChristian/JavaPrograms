53b444075ce57df73a1379f46e4ed967a6b67a0c
Maxwell Christian <112983367+MaxwellChristian@users.noreply.github.com>
2022-11-01 13:35:59 -0600

    Maxwell Christian

---
+++ src/array/ArrayUtilities.java
@@ -0,0 +1,101 @@
+package array;
+
+import java.util.Random;
+
+public class ArrayUtilities {
+
+    public static final int SORT_ASCENDING = 0;
+    public static final int SORT_DESCENDING = 1;
+
+    public static final int BUBBLE_SORT = 0;
+    public static final int SELECTION_SORT = 1;
+
+    public static int findMaximum(int[][] matrix) {
+
+        int max = matrix[0][0];
+
+        for (int[] rows : matrix) {
+            for (int colValue : rows) {
+                if (colValue > max) {
+                    max = colValue;
+                }
+            }
+        }
+
+        return max;
+    }
+
+    public static void printValues(int[][] matrix) {
+        for (int[] rows : matrix) {
+            for (int colValue : rows) {
+                System.out.print(colValue + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void fillWithRandomValues(int[][] matrix, int maxValue) {
+
+        for (int row = 0; row < matrix.length; row++) {
+            for (int col = 0; col < matrix[row].length; col++) {
+                matrix[row][col] = new Random().nextInt() % maxValue;
+            }
+        }
+
+    }
+
+    public static void fillWithRandomValues(int[] values, int maxValue, boolean positiveValueOnly) {
+        for (int index = 0; index < values.length; index++) {
+            values[index] = (positiveValueOnly) ? Math.abs(new Random().nextInt() % maxValue) : new Random().nextInt() % maxValue;
+        }
+    }
+
+    public static void printValues(int[] values) {
+        for (int currentValue : values) {
+            System.out.print(currentValue + " ");
+        }
+    }
+
+    public static void sort(int[] values, int sortOrder, int sortingAlgorithm) {
+
+        switch (sortingAlgorithm) {
+            case BUBBLE_SORT -> {
+                bubbleSort(values, sortOrder);
+            }
+            case SELECTION_SORT -> {
+                selectionSort(values, sortOrder);
+            }
+        }
+    }
+
+    private static void selectionSort(int[] values, int sortOrder) {
+
+    }
+
+    private static void bubbleSort(int[] values, int sortOrder) {
+        for (int index1 = 0; index1 < values.length; index1++) {
+            for (int index2 = index1 + 1; index2 < values.length; index2++) {
+                switch (sortOrder) {
+                    case SORT_ASCENDING -> {
+                        if (values[index1] > values[index2]) {
+                            int temp;
+                            temp = values[index1];
+                            values[index1] = values[index2];
+                            values[index2] = temp;
+                        }
+                    }
+                    case SORT_DESCENDING -> {
+                        if (values[index1] < values[index2]) {
+                            int temp;
+                            temp = values[index1];
+                            values[index1] = values[index2];
+                            values[index2] = temp;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+}
+
