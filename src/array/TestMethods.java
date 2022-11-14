927b752db62c519e744828b6cf9606a366f899ed
Maxwell Christian <112983367+MaxwellChristian@users.noreply.github.com>
2022-11-14 14:11:48 -0600

    Maxwell Christian

---
+++ src/array/TestMethods.java
@@ -0,0 +1,18 @@
+package array;
+
+public class TestMethods {
+
+    // find a value from an array
+
+    // join two arrays
+
+    // convert 2D to 1D array
+
+    // convert 3D to 1D array
+
+    // get values from array as per indices
+    // version 1
+
+    // get values from array as per indices
+    // version 2
+}
