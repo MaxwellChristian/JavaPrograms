f83ba1f4a347340e4d3c1b3388d1b13110a7d672
Maxwell Christian <112983367+MaxwellChristian@users.noreply.github.com>
2022-11-14 14:22:44 -0600

    Maxwell Christian

--- src/array/TestMethods.java
+++ src/array/TestMethods.java
@@ -3,16 +3,35 @@ package array;
 public class TestMethods {
 
     // find a value from an array
+    int findValue(int []arr, int valueToSearch){
+        // return the index of value from the array
+        // if not found then return -1
+    }
 
     // join two arrays
+    int[] joinArrays(int []arr1, int []arr2){
+        // returns a new int which has concatenated values of arr1 followed by arr2
+    }
 
     // convert 2D to 1D array
+    int[] convert(int [][]matrix){
+        // returns an array with all values from matrix
+    }
 
     // convert 3D to 1D array
+    int[] convert(int [][][] values){
+        // returns an array with all values from the 3D array
+    }
 
     // get values from array as per indices
     // version 1
+    int[] fetchValues(int []arr, int startIndex){
+        // returns an array having values starting from "startIndex" to end of the arr
+    }
 
     // get values from array as per indices
     // version 2
+    int[] fetchValues(int []arr, int startIndex, int endIndex){
+        // returns an array having values starting from "startIndex" to "endIndex" of the arr
+    }
 }
