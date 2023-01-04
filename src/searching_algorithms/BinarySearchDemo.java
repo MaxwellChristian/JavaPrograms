c275f5bf3b500b9d9dbeb4ce09f77e947a050675
Maxwell Christian <112983367+MaxwellChristian@users.noreply.github.com>
2023-01-04 10:21:18 -0600

    Maxwell Christian

--- src/searching_algorithms/BinarySearchDemo.java
+++ src/searching_algorithms/BinarySearchDemo.java
@@ -7,6 +7,15 @@ public class BinarySearchDemo {
     public static void main(String []args){
 
         int []values = {10, 9, 90, 181, -9, -89, 189, 29};
+
+        // print the unsorted list of values
+        ArrayUtilities.printValues(values);
+        System.out.println();
+
+        // sort the list of values using SELECTION sort
+        ArrayUtilities.sort(values, ArrayUtilities.SORT_ASCENDING, ArrayUtilities.BUBBLE_SORT);
+
+        // print the sorted list of values
         ArrayUtilities.printValues(values);
         System.out.println();
 
@@ -20,6 +29,45 @@ public class BinarySearchDemo {
         else {
             System.out.println(valueToSearch + " not found " );
         }
+
+        foundAtIndex = binarySearchRecursive(values, valueToSearch, 0, values.length-1);
+        if( foundAtIndex != -1 ){
+            System.out.println(valueToSearch + " found at index " + foundAtIndex );
+        }
+        else {
+            System.out.println(valueToSearch + " not found " );
+        }
+
+    }
+
+    private static int binarySearchRecursive(int[] values, int valueToSearch, int left, int right) {
+
+        // Restrict the bounds of
+        // right index and the left index
+        // to prevent overflow of indices
+        if (right >= left && left <= values.length - 1) {
+
+            int mid = left + (right - left) / 2;
+
+            // compare to check if
+            // search value is at mid-index
+            if (values[mid] == valueToSearch)
+                return mid;
+
+            // If search values is smaller than mid,
+            // then it can exist only in LEFT sub-list of values
+            // LEFT sub-list: change the right to mid - 1
+            if (valueToSearch > values[mid])
+                return binarySearchRecursive(values, valueToSearch, left, mid - 1);
+
+            // If search values is smaller than mid,
+            // then it can exist only in RIGHT sub-list of values
+            // RIGHT sub-list: change the left to mid + 1
+            return binarySearchRecursive(values, valueToSearch, mid + 1, right);
+        }
+
+        // search value does not exist in the list
+        return -1;
 
     }
 
@@ -36,7 +84,7 @@ public class BinarySearchDemo {
                 return mid;
 
             // If x greater, ignore left half
-            if (values[mid] < valueToSearch)
+            if (valueToSearch > values[mid])
                 left = mid + 1;
 
                 // If x is smaller,
