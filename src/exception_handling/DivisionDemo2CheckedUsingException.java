ac23949774fe2b2f7a51f5d99f3c2c9ddae21585
Maxwell Christian <112983367+MaxwellChristian@users.noreply.github.com>
2023-01-06 10:19:49 -0600

    Maxwell Christian

--- src/exception_handling/DivisionDemo2CheckedUsingException.java
+++ src/exception_handling/DivisionDemo2CheckedUsingException.java
@@ -22,6 +22,8 @@ public class DivisionDemo2CheckedUsingException {
                     + result);
 
         }
+        // a try can have multiple catch clauses
+        // but each catch can be associated only with ONE try
         catch (ArithmeticException ex) {
             // for handling any arithmetic exception
             System.out.println("Exception: an integer " +
@@ -31,21 +33,27 @@ public class DivisionDemo2CheckedUsingException {
             // handling any exception related to input mismatch
             System.out.println("Provide only integer");
         }
+        // the following catch with Exception class must be at last
         catch (Exception ex){
             // handling all other un-caught exceptions
-            // [But this catch must always be at last
+            // [But this catch must always be at last]
             System.out.println(ex.getMessage());
         }
         finally {
+            // the 'finally' block is always executed.
+            // i.e. whether exception occurs or not, finally is always executed
             System.out.println("In finally");
         }
 
+        // the following statement will be executed [whether exception occurs or not]
         System.out.println("Execution continues ...");
     }
 
     public static int divide(int number1, int number2) {
-        if (number2 == 0)
+        if (number2 == 0) {
             throw new ArithmeticException("Divisor cannot be zero");
+            // the 'throw' keyword is used to throw back the exception to the calling method
+        }
 
         return number1 / number2;
     }
