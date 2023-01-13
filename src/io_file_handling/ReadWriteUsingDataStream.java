bee14a2c3aa8ee1169fb4ea1aa87d0e1075f92ad
Maxwell Christian <112983367+MaxwellChristian@users.noreply.github.com>
2023-01-13 08:49:57 -0600

    Maxwell Christian

--- src/io_file_handling/ReadWriteUsingDataStream.java
+++ src/io_file_handling/ReadWriteUsingDataStream.java
@@ -15,7 +15,12 @@ public class ReadWriteUsingDataStream {
         try {
 
             // connect the data output stream with the destination file
-            DataOutputStream dataOut = new DataOutputStream(new FileOutputStream(destinationFile));
+            // opens the file in write/output mode [not append, overwrite mode]
+//            DataOutputStream dataOut = new DataOutputStream(new FileOutputStream(destinationFile));
+
+            // connect the data output stream with the destination file
+            // passing 'true' to the FileOutputStream constructor opens the file in 'append' mode
+            DataOutputStream dataOut = new DataOutputStream(new FileOutputStream(destinationFile, true));
 
             // write the enrolment number
             dataOut.writeInt(1);
@@ -37,11 +42,17 @@ public class ReadWriteUsingDataStream {
             // connect the data input stream with the source file
             DataInputStream dataInput = new DataInputStream(new FileInputStream(sourceFile));
 
-            System.out.println("Enrolment number: " +  dataInput.readInt());
-            System.out.println("First name: " +  dataInput.readUTF());
-            System.out.println("Middle name: " +  dataInput.readUTF());
-            System.out.println("Last name: " +  dataInput.readUTF());
-            System.out.println("Contact number: " +  dataInput.readLong());
+            // available() method returns the bytes available to read from the file
+            // so when it returns ZERO (i.e., no more bytes to read), we have reached the end of the file
+            while (dataInput.available() != 0){
+                System.out.print(dataInput.readInt() + " ");
+                System.out.print(dataInput.readUTF() + " ");
+                System.out.print(dataInput.readUTF() + " ");
+                System.out.print(dataInput.readUTF() + " ");
+                System.out.print(dataInput.readLong() + " ");
+
+                System.out.println();
+            }
 
         } catch (FileNotFoundException e) {
             System.out.println("File does not exist : " + e.getMessage());
