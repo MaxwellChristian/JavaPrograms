2c6888a562499f5cebd46a3d5939fa7217a24b1f
Maxwell Christian <112983367+MaxwellChristian@users.noreply.github.com>
2023-01-18 12:06:21 -0600

    Maxwell Christian

---
+++ src/generics_demos/LinkedListDemo.java
@@ -0,0 +1,30 @@
+package generics_demos;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+
+public class LinkedListDemo {
+
+    public static void main(String []args){
+
+        LinkedList<Integer> llInts = new LinkedList<>();
+
+        llInts.add(1);
+        llInts.add(2);
+        llInts.add(3);
+        llInts.add(4);
+        llInts.add(5);
+
+        System.out.println(llInts);
+
+        System.out.println("Last: " + llInts.getLast());
+        System.out.println("First: " + llInts.getFirst());
+
+        Iterator<Integer> llIterator = llInts.iterator();
+        while (llIterator.hasNext()){
+            System.out.println(llIterator.next());
+        }
+
+    }
+
+}
