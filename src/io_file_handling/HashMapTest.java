fa6ff91076f7e88116dc47cd6a9c6451b8eb0442
Maxwell Christian <112983367+MaxwellChristian@users.noreply.github.com>
2023-01-19 13:54:39 -0600

    Maxwell Christian

---
+++ src/io_file_handling/HashMapTest.java
@@ -0,0 +1,32 @@
+package io_file_handling;
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class HashMapTest {
+
+    public static void main(String[] args) {
+
+        // Create a HashMap
+        Map<String, Integer> hashMap = new HashMap<>();
+        hashMap.put("Smith", 30);
+        hashMap.put("Anderson", 31);
+        hashMap.put("Lewis", 29);
+        hashMap.put("Cook", 29);
+        hashMap.put("Lewis", 89);
+
+        System.out.println("Display entries in HashMap");
+        System.out.println(hashMap + "\n");
+
+        System.out.println("Age of 'Anderson': " + hashMap.get("Anderson"));
+        System.out.println("Age of 'Lewis': " + hashMap.get("Lewis"));
+
+        System.out.println("All keys in the hash map: ");
+        System.out.println(hashMap.keySet());
+
+
+    }
+
+}
