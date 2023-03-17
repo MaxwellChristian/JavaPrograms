cc6ba7b57f974c45a1ee8c9f8a4a52e741eddf5c
Maxwell Christian <112983367+MaxwellChristian@users.noreply.github.com>
2023-03-17 10:03:23 -0600

    Maxwell Christian

---
+++ src/parallel_programming/Demo1.java
@@ -0,0 +1,68 @@
+package parallel_programming;
+
+// create 3 threads
+// thread 1: prints letter 'A' 100 times
+// thread 2: prints letter 'B' 100 times
+// thread 3: prints numbers 1 to 100
+
+public class Demo1 {
+
+    public static void main(String[] args) {
+
+        // create the tasks
+        Runnable r1 = new ShowValue('A', 10);
+        Runnable r2 = new ShowValue('B', 10);
+        Runnable r3 = new ShowValue(10);
+
+        // create threads with assigned tasks
+        Thread t1 = new Thread(r1);
+        Thread t2 = new Thread(r2);
+        Thread t3 = new Thread(r3);
+
+        // start the threads [so that they can work]
+        t1.start();
+        t2.start();
+        t3.start();
+    }
+}
+
+class ShowValue implements Runnable {
+
+    boolean showLetter;
+    private char letter;
+    private int repeat;
+    public ShowValue(char letter, int repeat) {
+        showLetter = true;
+        this.letter = letter;
+        this.repeat = repeat;
+    }
+
+    public ShowValue(int repeat) {
+        showLetter = false;
+        this.repeat = repeat;
+    }
+
+    @Override
+    public void run() {
+
+        if( showLetter){
+            show(letter, repeat);
+        }
+        else {
+            show(repeat);
+        }
+
+    }
+
+    private void show(int repeat) {
+        for (int i = 0 ; i < repeat ; i++) {
+            System.out.print( (i+1) + " ");
+        }
+    }
+
+    private void show(char letter, int repeat) {
+        for (int i = 0 ; i < repeat ; i++) {
+            System.out.print( letter + " ");
+        }
+    }
+}
