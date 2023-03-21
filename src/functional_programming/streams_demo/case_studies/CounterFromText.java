a0a04ab52f12b238841e00e6a4d51963f7e11f77
Maxwell Christian <112983367+MaxwellChristian@users.noreply.github.com>
2023-03-21 14:15:43 -0600

    Maxwell Christian

--- src/functional_programming/streams_demo/case_studies/CounterFromText.java
+++ src/functional_programming/streams_demo/case_studies/CounterFromText.java
@@ -2,10 +2,31 @@ package functional_programming.streams_demo.case_studies;
 
 // using streams display the occurrence of each character in a text
 
+// modify to fetch the text from command line argument
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.TreeMap;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class CounterFromText {
 
-    public static void main(String []args) {
+    public static void main(String[] args) {
 
+        Arrays.stream(args)
+
+
+
+        ;
+
+        "Welcome".chars().mapToObj(value -> (char) value)
+                .filter(Character::isLetter)
+                .map(Character::toUpperCase)
+                .collect(Collectors.groupingBy(character -> character, TreeMap::new, Collectors.counting()))
+                .forEach((character, count) -> {
+                    System.out.println(character + " : " + count);
+                });
 
 
     }
